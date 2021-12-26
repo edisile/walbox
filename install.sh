a58387c353544b4ef86d3f47e28efe83ab2fdef0
Otavio Cornelio <otaviocos_13@hotmail.com>
2021-12-26 19:21:22 -0300

    Fix shellsheck errors

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ mkdir "$config_dir"/wal/templates/ 2> /dev/null
 cp themerc "$config_dir"/wal/templates/themerc
 
 # Generate a themerc from current wal colors
-wal -nqi $(cat $cache_dir/wal/wal)
+wal -nqi "$(cat "$cache_dir"/wal/wal)"
 
 # Copy theme bitmaps
 cp -r Walbox ~/.themes/Walbox
 
 # Symlink themerc
-ln --symbolic $cache_dir/wal/themerc $HOME/.themes/Walbox/openbox-3/themerc
+ln --symbolic "$cache_dir/wal/themerc" "$HOME/.themes/Walbox/openbox-3/themerc"
