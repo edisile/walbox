2150172208b3bb052b796c7555e05c9c30d0c68b
Otavio Cornelio <otaviocos_13@hotmail.com>
2022-01-29 19:31:18 -0300

    Create theme folder before copying

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 config_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
 cache_dir="${XDG_CACHE_DIR:-$HOME/.cache}"
+theme_dir="${XDG_CONFIG_HOME:-$HOME/.themes}"
 
 # Copy themerc template
 mkdir "$config_dir"/wal/templates/ 2> /dev/null
@@ -11,7 +12,8 @@ cp themerc "$config_dir"/wal/templates/themerc
 wal -nqi "$(< "$cache_dir"/wal/wal)"
 
 # Copy theme bitmaps
-cp -r Walbox ~/.themes/Walbox
+mkdir -p "$theme_dir"
+cp -r Walbox  "$theme_dir/Walbox"
 
 # Symlink themerc
-ln --symbolic "$cache_dir/wal/themerc" "$HOME/.themes/Walbox/openbox-3/themerc"
+ln --symbolic "$cache_dir/wal/themerc" "$theme_dir/Walbox/openbox-3/themerc"
